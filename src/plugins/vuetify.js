70eb6cfc864bc30173f7399b3aedf03a3348d68c
Parajuli Kiran <kiranparajuli589@gmail.com>
2022-01-12 23:53:20 +0545

    Lint fixes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import Vue from "vue"
+import Vuetify from "vuetify/lib/framework"
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
 	theme: {
@@ -20,4 +20,4 @@ export default new Vuetify({
 			},
 		},
 	},
-});
+})
