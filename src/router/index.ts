d44410cf729c9191c81d475778fbf2a055c5d955
Parajuli Kiran <kiranparajuli589@gmail.com>
2023-05-11 13:15:44 +0545

    Update chunk name

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import {createRouter, createWebHistory} from "vue-router"
 const routes = [
 	{
 		path: "/",
-		component: () => import("@/layout/HomeLayout.vue"),
+		component: () => import(/* webpackChunkName: "homeLayout" */ "@/layout/HomeLayout.vue"),
 		children: [
 			{
 				path: "",
