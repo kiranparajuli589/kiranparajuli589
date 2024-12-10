e6fd4fc8218d7feea0736da971fc8099f65bfe47
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2024-12-11 03:25:32 +0545

    build-n-push
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

---
+++ build_and_push.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+git checkout dist
+rm -rf assets index.html
+git add .
+git commit -m "Prepare dist ""$(date '+%Y-%m-%d')"
+git checkout main
+pnpm build
+git checkout dist
+mv dist/assets .
+mv dist/index.html .
+rm -rf dist
+git add .
+git commit --amend --no-edit
+git push
+git checkout main
+
+
+
