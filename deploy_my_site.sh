584a270cf7aa994fc20a8da2f026df1b5aa7edaf
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2024-12-11 03:52:01 +0545

    fix scripts
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- deploy_my_site.sh
+++ deploy_my_site.sh
@@ -3,6 +3,8 @@
 cd $HOME/kiranparajuli589 || exit
 git pull
 cd $HOME/kiranparajuli.com.np || exit
-rm -rf assets index.html
+rm -rf assets index.html blogBase blogs.json
 cp -r $HOME/kiranparajuli589/assets .
 cp $HOME/kiranparajuli589/index.html .
+cp $HOME/kiranparajuli589/blogBase .
+cp $HOME/kiranparajuli589/blogs.json .
